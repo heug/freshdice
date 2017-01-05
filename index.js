59d22332d0e4046cebf0f556c430f7775f5e271b
Eugene Choe <eugene.choe@gmail.com>
2017-01-04 23:58:26 -0800

    Complete scoring function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 class Game {
+
   constructor(players) {
     this.players = players;
     this.scores = {};
@@ -6,6 +7,7 @@ class Game {
 
   determineScore(arr) {
     var round = {};
+    var score = 0;
     for (var i = 0; i < arr.length; i++) {
       if (round[arr[i]]) {
         round[arr[i]]++;
@@ -15,12 +17,29 @@ class Game {
     }
     if (arr.length === 6) {
       if (Object.keys(round).length === 6) {
-        return console.log('You rolled a straight!');
+        score += 1500;
+        console.log('You rolled a straight! ' + score);
       }
+      var doublesCount = 0;
       for (var die in round) {
-        console.log(round[die]); 
+        if (round[die] === 2) {
+          doublesCount++;
+        }
+        if (doublesCount === 3) {
+          score += 1000;
+          console.log('You rolled three pair! ' + score);
+        }
       }
     }
+
+    for (var die in round) {
+      if (round[die] >= 3) {
+        score += die * (die == 1 ? 1000 : 100) * Math.max(round[die] - 2, 1);
+      } else if (die == 1 || die == 5) {
+        score += round[die] * die * (die == 1 ? 100 : 10);
+      }
+    }
+    console.log(score);
     console.log(round);
     
   }
@@ -35,9 +54,9 @@ class Game {
     //   results.push(roll);
     //   sentence += roll + ' ';
     // }
-    var results = [1,1,2,2,3,3];  
+    var results = [1,2,2,4,3,3];  
     // var results = [1,2,3,4,5,6];
-    console.log(sentence);
+    // console.log(sentence);
     this.determineScore(results);
   }
 }
