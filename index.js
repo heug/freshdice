800b3c53d426d0f8543df03d40ff899b1cd5fc22
Eugene Choe <eugene.choe@gmail.com>
2017-01-04 16:59:15 -0800

    Add determineScore fn

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 class Game {
   constructor(players) {
     this.players = players;
-    this.dice = 6;
+    this.scores = {};
   }
 
-  roll() {
-    console.log('rolling ' + this.dice + ' dice');
+  determineScore(arr) {
+    var round = {};
+    for (var i = 0; i < arr.length; i++) {
+      if (round[arr[i]]) {
+        round[arr[i]]++;
+      } else {
+        round[arr[i]] = 1;
+      }
+    }
+    if (arr.length === 6) {
+      if (Object.keys(round).length === 6) {
+        return console.log('You rolled a straight!');
+      }
+      for (var die in round) {
+        console.log(round[die]); 
+      }
+    }
+    console.log(round);
+    
+  }
+
+  roll(numDice) {
+    numDice = numDice || 6;
+    console.log('Rolling ' + numDice + ' dice');
+    // var results = [];
+    var sentence = 'You rolled ';
+    // for (var i = 0; i < numDice; i++) {
+    //   var roll = Math.ceil(Math.random() * 6);
+    //   results.push(roll);
+    //   sentence += roll + ' ';
+    // }
+    var results = [1,1,2,2,3,3];
+    // var results = [1,2,3,4,5,6];
+    console.log(sentence);
+    this.determineScore(results);
   }
 }
 
